dc6dac8afd8c72ec44787360030a9d561dbac09b
Rodrigo Mageste R. Pereira <rodrigo.mgst@gmail.com>
2013-06-04 19:48:05 -0300

    Adicionado link para edição do usuário e o campo "nome" na view de edição do devise

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -13,14 +13,14 @@ feature 'User' do
     fill_in 'Email', :with => 'rodrigomageste@gmail.com'
     fill_in 'Password', :with => '123456'
     click_button 'Sign in'
-    page.should have_content 'Signed in successfully.'
+    expect(page).to have_content 'Signed in successfully.'
   end
 
   scenario 'deve fazer logout' do
     login_as @user
     visit root_path
     click_link 'Sign out'
-    page.should have_content 'Signed out successfully.'
+    expect(page).to have_content 'Signed out successfully.'
   end
 
   scenario 'deve criar usuário' do
@@ -31,7 +31,21 @@ feature 'User' do
     fill_in 'Password', with: '123456'
     fill_in 'Password confirmation', with: '123456'
     click_button 'Sign up'
-    page.should have_content 'A message with a confirmation link has been sent
-     to your email address. Please open the link to activate your account.'
+    expect(page).to have_content 'A message with a confirmation link has been
+     sent to your email address. Please open the link to activate your account.'
+  end
+
+  scenario 'deve pode editar o usuario logado' do
+    login_as @user
+    visit root_path
+    click_link 'Edit'
+    fill_in 'Name', with: 'Rodrigo'
+    fill_in 'Password', with: '1234567'
+    fill_in 'Password confirmation', with: '1234567'
+    fill_in 'Current password', with: '123456'
+    click_button 'Update'
+    expect(page).to have_content 'You updated your account successfully.'
+    user = User.last
+    expect(user.name).to eq('Rodrigo')
   end
 end
