a69585d71428619cd1ed64a227f92d67e111049c
Pedro Henrique Passalini <henrique.passalini@gmail.com>
2013-06-05 11:13:14 -0300

    Criação básica de projeto

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 TimeTracking::Application.routes.draw do
+  
+  resources :projects,  only: [:new, :create, :show]
+
   devise_for :users
 
   root to: 'site#index'
---
+++ spec/features/manage_project_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+feature 'manage project' do
+  context 'new' do
+    scenario 'successfully' do
+      visit new_project_path
+      expect(page).to have_content 'New Project'
+
+      fill_in 'Name', with: 'project_1'
+      click_button 'Create Project'
+      expect(page).to have_content 'Project was successfully created.'
+    end
+
+    
+  end
+end
