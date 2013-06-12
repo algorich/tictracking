f2711e70b8c7db66db77f64cb32b740b8dc9500a
Rodrigo Mageste R. Pereira <rodrigo.mgst@gmail.com>
2013-06-12 12:15:17 -0300

    Adicionado botão de Stop

--- app/controllers/worktimes_controller.rb
+++ app/controllers/worktimes_controller.rb
@@ -1,13 +1,28 @@
 class WorktimesController < ApplicationController
   def create
-    @worktime = Worktime.new(begin: Time.now, user_id: current_user.id, task_id: params[:worktime][:task_id ])
+    if params[:commit] == 'Continue'
+      if Worktime.last.end != nil
 
-    if @worktime.save
-      redirect_to project_path(params[:worktime][:project_id]),
-       notice: 'Successfully'
+        @worktime = Worktime.new(begin: Time.now, user_id: current_user.id,
+         task_id: params[:worktime][:task_id ])
+
+          if @worktime.save
+            redirect_to project_path(params[:worktime][:project_id]),
+             notice: 'Successfully'
+          else
+            redirect_to project_path(params[:worktime][:project_id]),
+             notice: "Can't be created"
+          end
+
+      else
+        redirect_to project_path(params[:worktime][:project_id]), alert:
+        'Should stop worktime'
+      end
+
     else
-      redirect_to project_path(params[:worktime][:project_id]),
-       notice: "Can't be created"
+      @worktime = Worktime.last.update_attribute(:end, Time.now)
+      redirect_to project_path(params[:worktime][:project_id]), notice:'Deleted'
     end
+
   end
 end
--- spec/features/worktime_spec.rb
+++ spec/features/worktime_spec.rb
@@ -6,17 +6,35 @@ feature 'Worktime' do
     @user.confirm!
     login_as @user
     @project = create :project, users: [@user]
+    @task = create :task, project: @project
+    @worktime = create :worktime, user: @user, task: @task
+    visit project_path(@project.id)
+  end
+
+  after do
+    Timecop.return
   end
 
   context 'create' do
     scenario 'successfully create worktime' do
-      @task = create :task, project: @project
-      @worktime = create :worktime, user: @user, task: @task
-      visit project_path(@project.id)
       time = Time.local(2008, 9, 1, 10, 5, 0)
       Timecop.freeze(time)
-      click_button 'Begin'
+      click_button 'Continue'
       expect(page).to have_content '2008-09-01 13:05:00 UTC'
     end
   end
+
+  context 'stop' do
+    scenario 'successfully stop worktime' do
+      time = Time.local(2013, 6, 1, 10, 5, 0)
+      Timecop.freeze(time)
+      click_button 'Continue'
+      expect(page).to have_content 'Successfully'
+      time = Time.local(2013, 6, 1, 11, 5, 0)
+      Timecop.freeze(time)
+      click_button 'Stop'
+      expect(page).to have_content '2013-06-01 14:05:00 UTC'
+    end
+  end
+
 end
