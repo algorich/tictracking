326a46e0d972b9dc375c4d9d7626df55cf9d5d3f
Pedro Henrique Passalini <henrique.passalini@gmail.com>
2013-06-12 12:27:26 -0300

    criando dependência de worktime em task

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Task do
   it { should_not have_valid(:name).when(nil, '') }
   it { should_not have_valid(:project).when(nil) }
+  it { should have_many(:worktimes).dependent(:destroy) }
 
   it 'should_not have equal names' do
     Task.create name:'Ro', project: Project.new
