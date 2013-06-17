8379ac61920a5af9ed3a3bb34570615df2b5cad6
Pedro Henrique Passalini <henrique.passalini@gmail.com>
2013-06-17 16:22:43 -0300

    iniciado dashboard

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,10 @@
 class SiteController < ApplicationController
+  before_filter :authenticate_user!, only: [:dashboard]
+
   def index
+  end
+
+  def dashboard
+    @projects = current_user.projects.order('updated_at DESC').first(3)
   end
 end
