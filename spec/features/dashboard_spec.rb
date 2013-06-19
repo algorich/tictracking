99949c36ce68fe466afd2accf2a1537f4ab9d171
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2013-06-19 09:55:27 -0300

    testes passando

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -17,23 +17,23 @@ feature 'Dashboard' do
   end
 
   context 'Content' do
-    before(:each) do
-      login_as @user
-      visit dashboard_path
-    end
+    before(:each) { login_as @user }
+
     scenario 'should have links to create and list projects' do
+      visit dashboard_path
       expect(page).to have_link('My projects', href: projects_path)
       expect(page).to have_link('New project', href: new_project_path)
     end
 
     scenario 'should the 3 latest tasks and projects' do
-      8.times do |i|
-        project = create(:project, name: "Project #{i}", users: [@user] )
-      end
+      5.times { |i| create(:project, name: "Project #{i}", users: [@user] ) }
+      visit dashboard_path
 
-      expect(page).to have_content('Project 7')
-      expect(page).to have_content('Project 6')
-      expect(page).to have_content('Project 5')
+      expect(page).to have_content('Project 4')
+      expect(page).to have_content('Project 3')
+      expect(page).to have_content('Project 2')
+      expect(page).not_to have_content('Project 1')
+      expect(page).not_to have_content('Project 0')
     end
   end
 end
