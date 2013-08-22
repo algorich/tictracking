137770ffc68374c311070934c8ffcf2123aae946
Pedro Henrique Passalini <henrique.passalini@gmail.com>
2013-08-22 15:30:55 -0300

    Mudando local e configuração do alert fixed

--- app/assets/javascripts/app_alert_fixed_offset.js
+++
@@ -1,8 +0,0 @@
-// Cache selectors outside callback for performance.
-var $window = $(window),
-    $stickyEl = $('#app_flash_message'),
-    elTop = $stickyEl.offset().top;
-
-$window.scroll(function() {
-    $stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
-});
--- app/assets/javascripts/app_show_flash_message.js
+++ app/assets/javascripts/app_show_flash_message.js
@@ -1,5 +1,18 @@
-$old_alert = null;
+var $window = $(window),
+    $stickyEl = $('#app_flash_message'),
+    elTop = $stickyEl.offset().top,
+    $old_alert = null;
 
+// sticky functions
+$window.scroll(function() {
+    toggle_sticky_class($stickyEl);
+});
+
+function toggle_sticky_class (element) {
+    element.toggleClass('sticky', $window.scrollTop() > elTop);
+};
+
+// show flash_message functions
 function app_show_flash_message (type, text) {
   clean_up();
   if (text != '') {
@@ -15,6 +28,10 @@ function clean_up () {
   $('.alert').addClass('hide');;
   if ($old_alert != null) {
     $('#app_flash_message_container').html($old_alert);
+
+    // to be an sticky element
+    $stickyEl = $old_alert
+    toggle_sticky_class($stickyEl);
   };
 
   var $alert = $('#app_flash_message');
