669436c10f52effdf095ea013fc450d4d8c049e5
Pedro Henrique Passalini <henrique.passalini@gmail.com>
2013-08-22 12:02:49 -0300

    consertando flah vazio

--- app/assets/javascripts/app_show_flash_message.js
+++ app/assets/javascripts/app_show_flash_message.js
@@ -2,11 +2,13 @@ $old_alert = null;
 
 function app_show_flash_message (type, text) {
   clean_up();
-  var element = $('.alert')[0];
-  var $alert = $(element);
-  $alert.addClass('alert-' + type);
-  $alert.children('span').text(text);
-  $alert.removeClass('hide');
+  if (text != '') {
+    var element = $('.alert')[0];
+    var $alert = $(element);
+    $alert.addClass('alert-' + type);
+    $alert.children('span').text(text);
+    $alert.removeClass('hide');
+  };
 }
 
 function clean_up () {
