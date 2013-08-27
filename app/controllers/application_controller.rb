1bc2e55c5a1e4a92e3fae2165895b2932893ef4f
Pedro Henrique Passalini <henrique.passalini@gmail.com>
2013-08-27 14:04:49 -0300

    configuração para cancan funcionar com rails 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
 
   layout :layout_by_resource
 
+  before_filter do
+    resource = controller_path.singularize.gsub('/', '_').to_sym
+    method = "#{resource}_params"
+    params[resource] &&= send(method) if respond_to?(method, true)
+  end
+
   def set_latests_projects
     if current_user
       @latests_projects = current_user.projects.order('updated_at DESC').first(3)
@@ -22,4 +28,5 @@ class ApplicationController < ActionController::Base
       'application'
     end
   end
+
 end
